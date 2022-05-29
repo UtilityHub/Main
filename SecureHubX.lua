5a9a38b1c0d2925c8ceea2c2b06a36b5d2d6d7db
SecureHubX <106483689+SecureHubX@users.noreply.github.com>
2022-05-29 16:44:27 -0400

    Update SecureHubX.lua

--- SecureHubX.lua
+++ SecureHubX.lua
@@ -13,6 +13,5 @@ local games = {
     ["RebirthChampionsX"] = 8540346411,
     ["ClickerSimulator"] = 7560156054,
 }
-
 loadstring(game:HttpGet("https://raw.githubusercontent.com/SecureHubX/Main/main/%22..k))()%3B" .. k))();
 
